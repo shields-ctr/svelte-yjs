f1a2ddd9a4fea6bc24eebee612392fc322486c01
SHINN, MICHAEL L CTR USAF ACC 805 CTS/DOZ <michael.shinn.5.ctr@us.af.mil>
2025-05-09 15:46:55 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import viteLogo from '/vite.svg';
+	import { setContext } from 'svelte';
+	import Counter from './lib/Counter.svelte';
+
+	const clicks = $state({ count: 4 });
+
+	setContext('clicks', clicks);
+</script>
+
+<main class="grid place-content-center h-screen">
+	<div class="card shadow-sm bg-slate-700 p-5 pt-8">
+		<div class="ml-auto mr-auto">
+			<a href="https://vite.dev" target="_blank" rel="noreferrer">
+				<img src={viteLogo} class="logo" alt="Vite Logo" />
+			</a>
+		</div>
+		<div class="card-body">
+			<p><Counter></Counter></p>
+			<p><Counter></Counter></p>
+			<p><Counter></Counter></p>
+		</div>
+	</div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { getContext } from 'svelte';
+
+	let clicks = getContext('clicks');
+</script>
+
+<button
+	onclick={() => {
+		clicks.count++;
+	}}
+	class="btn"
+>
+	clicks: {clicks.count}
+</button>
