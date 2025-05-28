134b90d917032642b5a3a371a3ae919e701573c7
Casey Shields <casey.shields.1.ctr@us.af.mil>
2025-05-28 10:41:15 -0700

    trying(failing) to fix ts config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
-import * as Y from 'yjs';
-
-const doc = new Y.Doc();
 
 const app = mount(App, {
   target: document.getElementById('app')!,
